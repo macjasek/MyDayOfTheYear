08089908e26e5f12abaafc0a79da48b2e89b696b
macjasek <maciej.dobruchowski@ispro.com.pl>
2017-05-11 18:24:48 +0200

    Drobne zmiany dla poprawienia czytelności

--- MyDayOfTheYear/Program.cs
+++ MyDayOfTheYear/Program.cs
@@ -10,37 +10,38 @@ namespace MyDayOfTheYear
     {
         static void Main(string[] args)
         {
-            //Finding day of the year not using method DayOfTheYear available in DateTime
             Console.WriteLine("Podaj rok urodzenia");
-            var year = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
             Console.WriteLine("Podaj miesiac urodzenia");
-            var month = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+
             while (month < 1 || month > 12)
             {
-                Console.WriteLine("Miesiąc powinien być liczbą w przedziale od 1 do 12");
+                Console.WriteLine("Miesiac powinien byc liczba w przedziale od 1 do 12");
                 month = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("Podaj dzień swoich urodzin");
-            var day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj dzien swoich urodzin");
+            int day = int.Parse(Console.ReadLine());
 
             var birthday = new DateTime(year,month,day);
             var dayZero = new DateTime(year - 1, 12, 31);
 
-            var myDayOfTheYear = (birthday - dayZero).Days;
+            int myDayOfTheYear = (birthday - dayZero).Days;
 
-            var yesno = " nie";
+            string yesno = " nie";
 
+            //sprawdzenie czy rok przestępny
             var day1 = new DateTime(year, 3, 1);
             var day2 = new DateTime(year, 2, 28);
-            var daycheck = day1 - day2;
+            TimeSpan daycheck = day1 - day2;
 
             if (daycheck.Days > 1 )
             {
                 yesno = "";
             }
 
-            Console.WriteLine($"Urodziłeś się {myDayOfTheYear} dnia roku i{yesno} był to rok przestępny.");
+            Console.WriteLine($"Dzien Twoich urodzin byl {myDayOfTheYear} dniem roku i{yesno} byl to rok przestępny.");
 
         }
     }
